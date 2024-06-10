ff9e14940a09fdf02342b11e70a7fc6ac94f704e
luccascgs <luccascorreia.08@hotmail.com>
2024-06-10 14:47:34 -0300

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import "react-native-gesture-handler";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  useFonts,
+  Montserrat_400Regular,
+  Montserrat_500Medium,
+  Montserrat_700Bold,
+} from "@expo-google-fonts/montserrat";
+import Routes from "./src/router";
+export default function App() {
+  let [fontsLoaded] = useFonts({
+    Montserrat_400Regular,
+    Montserrat_500Medium,
+    Montserrat_700Bold,
+  });
+  return (
+    <>
+      <StatusBar style="light" backgroundColor="#000" translucent={false} />
+      <Routes />
+    </>
+  );
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+Tela Principal
+
+![Tela Principal - Alugar Casas](https://github.com/JonatanGoncalves/HouseApp/assets/136382063/8d2497b9-7703-4ff4-999b-d2453d990e0b)
+
+-------
+Tela De Detalhes
+
+![image](https://github.com/JonatanGoncalves/HouseApp/assets/136382063/8af03a23-d4cd-4ed5-bafc-f5154119c08f)
